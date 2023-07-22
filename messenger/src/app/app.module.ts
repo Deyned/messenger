d8ce2b374f011605e1de4bd278920379be423638
Andrey Pavlov <39939511+Deyned@users.noreply.github.com>
2023-07-22 16:43:29 +0500

    Добавления возможности деплоя в firebase (#5)

--- messenger/src/app/app.module.ts
+++ messenger/src/app/app.module.ts
@@ -6,6 +6,38 @@ import { AppComponent } from './app.component';
 import { RegistrationModule } from './pages/registration/registration.module';
 import { AuthorizationModule } from './pages/authorization/authorization.module';
 import { LayoutComponent } from './common/components/layout/layout.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import {
+	provideAnalytics,
+	getAnalytics,
+	ScreenTrackingService,
+	UserTrackingService,
+} from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { providePerformance, getPerformance } from '@angular/fire/performance';
+import {
+	provideRemoteConfig,
+	getRemoteConfig,
+} from '@angular/fire/remote-config';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+const firebaseModules = [
+	provideFirebaseApp(() => initializeApp(environment.firebase)),
+	provideAnalytics(() => getAnalytics()),
+	provideAuth(() => getAuth()),
+	provideDatabase(() => getDatabase()),
+	provideFirestore(() => getFirestore()),
+	provideFunctions(() => getFunctions()),
+	provideMessaging(() => getMessaging()),
+	providePerformance(() => getPerformance()),
+	provideRemoteConfig(() => getRemoteConfig()),
+	provideStorage(() => getStorage()),
+];
 
 @NgModule({
 	declarations: [
@@ -18,8 +50,9 @@ import { LayoutComponent } from './common/components/layout/layout.component';
 		AppRoutingModule,
 		RegistrationModule,
 		AuthorizationModule,
+		...firebaseModules,
 	],
-	providers: [],
+	providers: [ScreenTrackingService, UserTrackingService],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ messenger/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	firebase: {
+		projectId: 'messenger-60e19',
+		appId: '1:418587140425:web:76c3a65f7e96ad1f41ba98',
+		storageBucket: 'messenger-60e19.appspot.com',
+		apiKey: 'AIzaSyDWZrVwSYG2dJIYSrUWkrNx9Pl1hHAyYpc',
+		authDomain: 'messenger-60e19.firebaseapp.com',
+		messagingSenderId: '418587140425',
+		measurementId: 'G-K9H73456LT',
+	},
+};
